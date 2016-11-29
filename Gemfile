3d60e347a952dda00b2fdb2685f3d67c6f9a54a7
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-29 12:40:35 +0000

    Add a sinatra api

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+ruby '2.3.1'
+
+gem 'sinatra'
+gem 'puma'
+gem 'foreman'
+gem 'dalli'
+
+group :development do
+  gem 'capistrano'
+end
---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+require './pydeps'
+require 'json'
+
+class PydepsAPI < Sinatra::Base
+  get '/:name/:version.json' do
+    content_type :json
+    pd = Pydeps::Resolver.new(params[:name], params[:version])
+    if pd.find_dependencies
+      pd.find_dependencies.to_json
+    else
+      {error: "Can't calculate dependencies for #{params[:name]} #{params[:version]}"}.to_json
+    end
+  end
+end
