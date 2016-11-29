89488e91aaebbbbc363192505f1a6a6dc62c633b
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-29 14:36:07 +0000

    Cache dependencies to memcached, fixes #1

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class PydepsAPI < Sinatra::Base
   get '/:name/:version.json' do
     content_type :json
     pd = Pydeps::Resolver.new(params[:name], params[:version])
-    if pd.find_dependencies
+    if pd.find_dependencies && pd.find_dependencies != "err"
       pd.find_dependencies.to_json
     else
       {error: "Can't calculate dependencies for #{params[:name]} #{params[:version]}"}.to_json
--- pydeps.rb
+++ pydeps.rb
@@ -10,6 +10,39 @@ module Pydeps
       @version = version
     end
 
+    def find_dependencies
+      memcached_client.fetch(cache_key) do
+        output = run_with_fallback
+        output ? parse(output) : "err"
+      end
+    end
+
+    private
+
+    def memcached_client
+      Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "localhost:11211").split(","),
+        {
+          username: ENV["MEMCACHIER_USERNAME"],
+          password: ENV["MEMCACHIER_PASSWORD"],
+          failover: true,
+          socket_timeout: 1.5,
+          socket_failure_delay: 0.2
+        })
+    end
+
+    def cache_key
+      [name, version].join('-')
+    end
+
+    def parse(output)
+      output.split("\n")
+    end
+
+    def command(mirror = true)
+      mirror_flags = mirror ? "-i http://pypi.libraries.io/simple --no-binary :all: --trusted-host pypi.libraries.io" : ""
+      "pip download #{name}==#{version} -d /tmp #{mirror_flags} | grep 'from #{name}' | cut -d' ' -f2"
+    end
+
     def run_command(mirror = true)
       _stdin, stdout, stderr = Open3.popen3(command(mirror))
       { err: stderr.read, res: stdout.read }
@@ -20,20 +53,6 @@ module Pydeps
       return with_mirror[:res] if with_mirror[:err].empty?
       without_mirror = run_command(false)
       return with_mirror[:res] if without_mirror[:err].empty?
-    end
-
-    def find_dependencies
-      output = run_with_fallback
-      parse(output) if output
-    end
-
-    def parse(output)
-      output.split("\n")
-    end
-
-    def command(mirror = true)
-      mirror_flags = mirror ? "-i http://pypi.libraries.io/simple --no-binary :all: --trusted-host pypi.libraries.io" : ""
-      "pip download #{name}==#{version} -d /tmp #{mirror_flags} | grep 'from #{name}' | cut -d' ' -f2"
     end
   end
 end
